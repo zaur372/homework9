36e8b1ee091560a13cce8afc7986b28cb4cf4dc2
zmurtazaliev <zaur.murtazaliev@ptnl.moscow>
2022-05-13 11:36:47 +0300

    homework9

---
+++ jest.local.js
@@ -0,0 +1,14 @@
+module.exports = {
+  testEnvironment: 'node',
+  reporters: [
+    'default'],
+  moduleFileExtensions: ['js', 'json'],
+  transform: {
+    '^.+\\.jsx?$': 'babel-jest',
+  },
+  testMatch: ['**/specs/*.spec.*'],
+  globals: {
+    testTimeout: 50000,
+  },
+  verbose: true,
+};
